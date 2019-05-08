0f96d29253f39d00b6f7b1d77b8ec86e37cabd51
vijitkamboj <vijitkamboj92@gmail.com>
2019-05-09 02:11:50 +0530

    Added Simple todo list

--- DOM Manipulation/script.js
+++ DOM Manipulation/script.js
@@ -1,9 +1,27 @@
 var container = document.querySelector("#container");
 var content = document.querySelector("#content");
 var header = document.querySelector("header");
-var h = document.querySelector(".h");
+var heading = document.querySelector(".heading");
+var input = document.querySelector("input");
 var ul = document.querySelector("ul");
-var li = document.querySelectorAll("l1");
 
 
-console.log(container);
+function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value="";
+
+}
+
+function addListAfterKeyPress(event){
+    if (inputLength() > 0 && event.keyCode === 13 ){
+        createListElement();
+    }
+}
+
+input.addEventListener("keypress",addListAfterKeyPress);
