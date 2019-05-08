3664836c79ad423582ce21ff512bfdf6c33390ef
vijitkamboj <vijitkamboj92@gmail.com>
2019-05-08 22:26:23 +0530

    added variables

---
+++ DOM Manipulation/script.js
@@ -0,0 +1,9 @@
+var container = document.querySelector("#container");
+var content = document.querySelector("#content");
+var header = document.querySelector("header");
+var h = document.querySelector(".h");
+var ul = document.querySelector("ul");
+var li = document.querySelectorAll("l1");
+
+
+console.log(container);
