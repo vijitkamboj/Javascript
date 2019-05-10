a2ca0ec4a7d9376085138cd7452ab0d8e06af569
vijitkamboj <vijitkamboj92@gmail.com>
2019-05-11 01:16:20 +0530

    Update Javascrpt Basics.js

---
+++ Javascrpt Basics.js
@@ -0,0 +1,26 @@
+// JavaScript DataTypes:
+
+    var Int= 1 //Integer;
+
+    var Str = "Vijit" //String;
+
+    var Bool1 = true ; //Bollean
+    var Bool0 = flase;
+
+    var Und ; //When there is nothing to return undefined is returned
+
+    var N = null; //No value;
+
+    var Obj = {  
+        a:500,
+        b:"Vijit",
+        c:true,
+        d:null
+    };           // Object
+
+
+// JavaScript DataStructures :
+
+    var arr = [2,"Vijit",true,null];
+
+    // Array is a object but with keys as indices[0,len-1]
